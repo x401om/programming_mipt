c34c159c637b45a9f1c7955612a78a23390637f8
Aleksey Goncharov <x401om@gmail.com>
2016-12-15 00:25:20 +0300

    Possible 0.5 points

--- Task_1/main.py
+++ Task_1/main.py
@@ -1,5 +1,8 @@
 import sys
 import csv
+import re
+
+REFERENCE_REGEX = r'([A-Z][0-9]+)'
 
 def loadTableFromFile(filePath):
     table = []
@@ -20,22 +23,56 @@ def basicTypeFromString(s):
     try:
         return int(s)
     except ValueError:
-        return float(s)
+        try:
+            return float(s)
+        except ValueError:
+            return s
 
-# Функция заменяет простые строчки на простые типы int и float, если в ячейке не формула
-def replaceBasicTypesInTable(table):
-    for i in range(0, len(table)):
-        for j in range(0, len(table[i])):
-            value = table[i][j]
-            if value[0] != '=':
-                table[i][j] = basicTypeFromString(value)
-    return table
+def isBasicExpression(string):
+    tokens = re.split(REFERENCE_REGEX, string[1:])[1:-1]
+    return len(tokens) == 0
 
 # Функция выдает результат вычисления выражения (формулы)
 def evaluateBasicExpression(expression):
     return eval(expression)
 
-# Функция просчитывает значения для всех ячеек
+# Функция заменяет простые строчки на простые типы int и float, если в ячейке формула без ссылок или число
+def replaceBasicTypesAndExpressionsInTable(table):
+    for i in range(0, len(table)):
+        for j in range(0, len(table[i])):
+            value = table[i][j]
+            if value[0] == '=':
+                if isBasicExpression(value):
+                    table[i][j] = evaluateBasicExpression(value[1:])
+            else:
+                table[i][j] = basicTypeFromString(value)
+    return table
+
+# Функция выдает координаты ячейки по ее идентификатору (e.g. А1 = 0,0, B1 = 0,1 ...)
+def tableCoordinateForLink(string):
+    return (basicTypeFromString(string[1:]) - 1, ord(string[0]) - ord('A'))
+
+# Функция превращает формулы со ссылками в обычные и считает их
+def replaceReferencesInTable(table):
+    pattern = re.compile(REFERENCE_REGEX)
+    for i in range(0, len(table)):
+        for j in range(0, len(table[i])):
+            value = table[i][j]
+            if isinstance(value, str):
+                exp = []
+                tokens = re.split(REFERENCE_REGEX, value[1:])[1:-1]
+                if len(tokens) > 0 :
+                    for token in tokens:
+                        if pattern.match(token):  # Это ссылка
+                            link = tableCoordinateForLink(token)
+                            exp.append(str(table[link[0]][link[1]]))
+                        else:
+                            exp.append(token)
+                    exp = ''.join(exp)
+                    table[i][j] = evaluateBasicExpression(exp)
+    return table
+
+
 def computedTableFromTable(table):
     for i in range(0, len(table)):
         for j in range(0, len(table[i])):
@@ -47,6 +84,6 @@ def computedTableFromTable(table):
 inputFilePath = sys.argv[1]
 outputFilePath = sys.argv[2]
 table = loadTableFromFile(inputFilePath)
-table = replaceBasicTypesInTable(table)
-table = computedTableFromTable(table)
+table = replaceBasicTypesAndExpressionsInTable(table)
 saveTableToFilePath(table, outputFilePath)
+table = replaceReferencesInTable(table)
